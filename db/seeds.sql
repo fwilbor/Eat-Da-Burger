0fc1338db427ff014a219e3caa78ccba9d187e4e
fwilbor <franklin.cfg@gmail.com>
2019-08-30 22:33:42 -0400

    setup orm.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheese-Burger Double Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("BBQ Burger Death Dog");
+INSERT INTO burgers (burger_name) VALUES ("Hamburger Mayo Only")
