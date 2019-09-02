15c3edf93859984e6328251d52b9dfa6e4b3a20e
fwilbor <franklin.cfg@gmail.com>
2019-09-02 01:51:08 -0400

    made change to server.js to route bruger_controller to correct path

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,9 +18,9 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (res) {
         // Send back the ID of the new quote
-        res.json({ id: result.insertId });
+        res.json({ id: res.insertId });
     });
 });
 
