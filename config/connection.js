c8330a511ef47523bd71926e80f229cd175e6926
fwilbor <franklin.cfg@gmail.com>
2019-09-07 12:36:56 -0400

    update connection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ if (process.env.JAWSDB_URL) {
 
     var connection = mysql.createConnection({
         host: "localhost",
-        port: 3306,
         user: "root",
         password: "Dramatiks33#",
         database: "burgers_db"
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese-Burger Double Deluxe", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger Death Dog", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger Mayo Only", false);
+INSERT INTO burgers (burger_name ) VALUES ("Bacon Cheese-Burger Double Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("BBQ Burger Death Dog");
+INSERT INTO burgers (burger_name ) VALUES ("Hamburger Mayo Only");
